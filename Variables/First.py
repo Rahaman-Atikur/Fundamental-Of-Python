bc690d1fcb2caa4ce04d67cd52e4e9f4b3850855
Atikur Rahaman <ar22acu@herts.ac.uk>
2024-11-11 09:04:19 +0000

    First Commit

---
+++ Variables/First.py
@@ -0,0 +1,11 @@
+a = 20
+b = 30
+name ="Phitron"
+_="Chain"
+mylist=["Potato","Onion",2000]
+num1 = 3
+num2 = 5
+print(a+b)
+print(name)
+print(_)
+print(mylist)
---
+++ userInput/userInput.py
@@ -0,0 +1,4 @@
+userName=input("Enter Your Name: ")
+age = input("Enter your age: ")
+
+print("Hellow",userName,"Your age is: ",age)

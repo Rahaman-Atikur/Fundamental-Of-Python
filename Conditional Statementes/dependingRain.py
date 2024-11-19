5ce5cd248d60c36fd47688a875253cd593fdbb05
Atikur Rahaman <ar22acu@herts.ac.uk>
2024-11-19 14:58:49 +0000

    First Commit

---
+++ Conditional Statementes/dependingRain.py
@@ -0,0 +1,14 @@
+rain = True
+if rain == False:
+    print("I am going to school")
+else:
+    print("I am not going to school")
+
+# Problem 2
+
+age = 12
+if age> 18:
+    print("You are getting the license")
+else:
+    print("You are not getting the license")
+    
